32fb1bc80e076c28ff2242e60295c22bd566e056
João Rezende <joaozinhobent@gmail.com>
2022-08-31 20:08:08 -0300

    Implementar BR

--- Executor.java
+++ Executor.java
@@ -7,28 +7,43 @@ class Executor {
     Executor(int[] memoria) {
         
         while ((registradorDeInstrucao = memoria[contadorDePrograma++]) != 11) {
-            if ((registradorDeInstrucao & 1111) == 02) { // ADD
-                if ((registradorDeInstrucao & 0b10000000) > 0) { // endereçamento imediato
-                    acumulador += memoria[contadorDePrograma++];
-                } else if ((registradorDeInstrucao & 0b100000) > 0) { // endereçamento indireto
-                    int endereco_do_endereco = memoria[contadorDePrograma++];
-                    int endereco = memoria[endereco_do_endereco];
-                    acumulador += memoria[endereco];
-                } else { // endereçamento direto
-                    int endereco = memoria[contadorDePrograma++];
-                    acumulador += memoria[endereco];
-                }
+            switch (registradorDeInstrucao & 1111) {
+                case 2: // ADD
+                    if ((registradorDeInstrucao & 0b10000000) > 0) { // endereçamento imediato
+                        acumulador += memoria[contadorDePrograma++];
+                    } else if ((registradorDeInstrucao & 0b100000) > 0) { // endereçamento indireto
+                        int enderecoDoEndereco = memoria[contadorDePrograma++];
+                        int endereco = memoria[enderecoDoEndereco];
+                        acumulador += memoria[endereco];
+                    } else { // endereçamento direto
+                        int endereco = memoria[contadorDePrograma++];
+                        acumulador += memoria[endereco];
+                    }
+                    break;
+                case 0: // BR
+                    if ((registradorDeInstrucao & 0b100000) > 0) { // endereçamento indireto
+                        int enderecoDoEndereco = memoria[contadorDePrograma++];
+                        contadorDePrograma = memoria[enderecoDoEndereco];
+                    } else { // endereçamento direto
+                        contadorDePrograma = memoria[contadorDePrograma];
+                    }
+                    break;
+                default:
+                    System.out.println("Instrução não implementada.");
+                    System.exit(1);
+            }
         }
 
     }
 
     public static void main(String[] args) {
         int[] memoria = new int[10_000];
-        memoria[0] =  2 | 0b100000;
-        memoria[1] = 3;
-        memoria[2] = 11;
-        memoria[3] = 4;
-        memoria[4] = 49;
+        memoria[0] = 0 | 0b100000;
+        memoria[1] = 5;
+        memoria[2] = 2 | 0b10000000;
+        memoria[3] = 49;
+        memoria[4] = 11;
+        memoria[5] = 2;
 
         Executor executor = new Executor(memoria);
 
