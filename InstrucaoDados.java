7488c2bdef58f60133d7434c56d7ab116ab6490a
João Rezende <joaozinhobent@gmail.com>
2022-09-30 19:03:52 -0300

    Popular Instrucoes e InstrucaoDados

--- InstrucaoDados.java
+++ InstrucaoDados.java
@@ -1,11 +1,32 @@
 public enum InstrucaoDados {
-    ADD((short) 2, 2);
+    ADD(2),
+    BR(2),
+    BRNEG(2),
+    BRPOS(2),
+    BRZERO(2),
+    CALL(2),
+    COPY(3),
+    DIVIDE(2),
+    LOAD(2),
+    MULT(2),
+    READ(2),
+    RET(1),
+    STOP(1),
+    STORE(2),
+    SUB(2),
+    WRITE(2),
+    CONST(1),
+    END(0),
+    EXTDEF(0),
+    EXTR(0),
+    SPACE(1),
+    STACK(0),
+    START(0)
+    ;
 
-    public short opcode;
     public int tamanho;
 
-    private InstrucaoDados(short opcode, int tamanho) {
-        this.opcode = opcode;
+    private InstrucaoDados(int tamanho) {
         this.tamanho = tamanho;
     }
 }
--- Instrucoes.java
+++ Instrucoes.java
@@ -5,6 +5,28 @@ public class Instrucoes {
 
     static {
         mapa.put("ADD", InstrucaoDados.ADD);
+        mapa.put("BR", InstrucaoDados.BR);
+        mapa.put("BRNEG", InstrucaoDados.BRNEG);
+        mapa.put("BRPOS", InstrucaoDados.BRPOS);
+        mapa.put("BRZERO", InstrucaoDados.BRZERO);
+        mapa.put("CALL", InstrucaoDados.CALL);
+        mapa.put("COPY", InstrucaoDados.COPY);
+        mapa.put("DIVIDE", InstrucaoDados.DIVIDE);
+        mapa.put("LOAD", InstrucaoDados.LOAD);
+        mapa.put("MULT", InstrucaoDados.MULT);
+        mapa.put("READ", InstrucaoDados.READ);
+        mapa.put("RET", InstrucaoDados.RET);
+        mapa.put("STOP", InstrucaoDados.STOP);
+        mapa.put("STORE", InstrucaoDados.STORE);
+        mapa.put("SUB", InstrucaoDados.SUB);
+        mapa.put("WRITE", InstrucaoDados.WRITE);
+        mapa.put("CONST", InstrucaoDados.CONST);
+        mapa.put("END", InstrucaoDados.END);
+        mapa.put("EXTDEF", InstrucaoDados.EXTDEF);
+        mapa.put("EXTR", InstrucaoDados.EXTR);
+        mapa.put("SPACE", InstrucaoDados.SPACE);
+        mapa.put("STACK", InstrucaoDados.STACK);
+        mapa.put("START", InstrucaoDados.START);
     }
 
     static InstrucaoDados getInstrucao(String nomeDaInstrucao) {
