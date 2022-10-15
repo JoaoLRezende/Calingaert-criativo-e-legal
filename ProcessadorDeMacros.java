fee1760d3da1412e89ce0dd5e740bcef72bcb161
João Rezende <joaozinhobent@gmail.com>
2022-10-15 11:04:48 -0300

    ProcMacros: ler definição de macro

--- ProcessadorDeMacros.java
+++ ProcessadorDeMacros.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.lang.reflect.Parameter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -12,6 +13,7 @@ public class ProcessadorDeMacros {
         String[] parâmetros;
         public Macro(String[] parâmetros) {
             this.parâmetros = parâmetros;
+            corpo = new String();
         }
 
         String corpo;
@@ -25,19 +27,22 @@ public class ProcessadorDeMacros {
         boolean modoDeDefinição = false;
         boolean modoDeExpansão = false;
         HashMap<String, Macro> tabelaDeMacros = new HashMap<>();
+        Macro macroSendoDefinida = null;
         while (fileScanner.hasNext()) {
             String linha = fileScanner.nextLine();
             Scanner lineScanner = new Scanner(linha);
 
             String opcode = lineScanner.next();
             // se opcode for na verdade um rótulo
-            if (!opcode.equals("MACRO") && !Instrucoes.existeOpname(opcode) && !tabelaDeMacros.containsKey(opcode)) {
+            if (!opcode.equals("MACRO") && !opcode.equals("MEND") && !Instrucoes.existeOpname(opcode) && !tabelaDeMacros.containsKey(opcode)) {
                 opcode = lineScanner.next();
             }
 
             if (modoDeDefinição) {
                 if (opcode.equals("MEND")) {
                     modoDeDefinição = false;
+                } else {
+                    macroSendoDefinida.corpo += substituiReferenciasAParâmetros(linha, macroSendoDefinida.parâmetros) + "\n";
                 }
 
             } else {
@@ -49,11 +54,32 @@ public class ProcessadorDeMacros {
                     String nomeDaMacro = tokensPrototipo[0];
                     String[] parâmetros = Arrays.copyOfRange(tokensPrototipo, 1, Math.max(2, tokensPrototipo.length));
 
-                    tabelaDeMacros.put(nomeDaMacro, new Macro(parâmetros));
+                    macroSendoDefinida = new Macro(parâmetros);
+                    tabelaDeMacros.put(nomeDaMacro, macroSendoDefinida);
                 }
             }
         }
         System.out.println(tabelaDeMacros);
+    }
+
+    static String substituiReferenciasAParâmetros(String linha, String[] parâmetros) {
+        ArrayList<String> listaParâmetros = new ArrayList<String>(Arrays.asList(parâmetros));
+
+        String novaLinha = "";
+        Scanner scanner = new Scanner(linha);
+        while (scanner.hasNext()) {
+            String token = scanner.next();
+
+            int índiceParâmetro = listaParâmetros.indexOf(token);
+            if (índiceParâmetro != -1) {
+                novaLinha += "#" + (índiceParâmetro + 1) + " ";
+            } else {
+                novaLinha += token + " ";
+            }
+
+        }
+        scanner.close();
+        return novaLinha.trim();
     }
 
     public static void main(String[] args) throws FileNotFoundException  {
