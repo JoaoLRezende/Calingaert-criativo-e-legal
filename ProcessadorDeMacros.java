8cf6111ec7649a1de4dfc2934ad830e27ef25643
João Rezende <joaozinhobent@gmail.com>
2022-10-17 19:24:56 -0300

    ProcMacros: ler definições aninhadas

--- ProcessadorDeMacros.java
+++ ProcessadorDeMacros.java
@@ -11,6 +11,7 @@ public class ProcessadorDeMacros {
 
     static class Macro {
         String[] parâmetros;
+
         public Macro(String[] parâmetros) {
             this.parâmetros = parâmetros;
             corpo = new String();
@@ -18,13 +19,13 @@ public class ProcessadorDeMacros {
 
         String corpo;
     }
-    
+
     static void executar(File moduloEntrada, File moduloSaida) throws FileNotFoundException {
         Scanner fileScanner = new Scanner(moduloEntrada);
 
         PrintStream outStream = new PrintStream(moduloSaida);
-        
-        boolean modoDeDefinição = false;
+
+        int nivelDeDefinição = 0;
         HashMap<String, Macro> tabelaDeMacros = new HashMap<>();
         Macro macroSendoDefinida = null;
         int númeroDeExpansões = 0;
@@ -38,38 +39,54 @@ public class ProcessadorDeMacros {
 
             String opcode = lineScanner.next();
             // se opcode for na verdade um rótulo
-            if (!opcode.equals("MACRO") && !opcode.equals("MEND") && !Instrucoes.existeOpname(opcode) && !tabelaDeMacros.containsKey(opcode)) {
+            if (!opcode.equals("MACRO") && !opcode.equals("MEND") && !Instrucoes.existeOpname(opcode)
+                    && !tabelaDeMacros.containsKey(opcode)) {
                 opcode = lineScanner.next();
             }
 
-            if (modoDeDefinição) {
-                if (opcode.equals("MEND")) {
-                    modoDeDefinição = false;
+            if (opcode.equals("MACRO")) {
+                nivelDeDefinição += 1;
+                String protótipo = fileScanner.nextLine();
+                String[] tokensPrototipo = protótipo.trim().split("\\s+");
+
+                String nomeDaMacro = tokensPrototipo[0];
+                String[] parâmetros = Arrays.copyOfRange(tokensPrototipo, 1, Math.max(2, tokensPrototipo.length));
+
+                if (nivelDeDefinição == 1) {
+                    macroSendoDefinida = new Macro(parâmetros);
+                    tabelaDeMacros.put(nomeDaMacro, macroSendoDefinida);
                 } else {
-                    macroSendoDefinida.corpo += substituiReferenciasAParâmetros(linha, macroSendoDefinida.parâmetros) + "\n";
+                    macroSendoDefinida.corpo += "MACRO\n";
+                    macroSendoDefinida.corpo += protótipo + "\n";
+                }
+            }
+
+            if (nivelDeDefinição > 0) {
+                if (opcode.equals("MEND")) {
+                    nivelDeDefinição -= 1;
+                } else if (nivelDeDefinição > 1) {
+                    macroSendoDefinida.corpo += substituiReferenciasAParâmetros(linha, macroSendoDefinida.parâmetros)
+                            + "\n";
+                }
+
+                if (opcode.equals("MEND") && nivelDeDefinição > 0) {
+                    macroSendoDefinida.corpo += "MEND\n";
                 }
 
             } else { // se não estamos em modo de definição
-                if (opcode.equals("MACRO")) {
-                    modoDeDefinição = true;
-                    String protótipo = fileScanner.nextLine();
-                    String[] tokensPrototipo = protótipo.trim().split("\\s+");
-
-                    String nomeDaMacro = tokensPrototipo[0];
-                    String[] parâmetros = Arrays.copyOfRange(tokensPrototipo, 1, Math.max(2, tokensPrototipo.length));
-
-                    macroSendoDefinida = new Macro(parâmetros);
-                    tabelaDeMacros.put(nomeDaMacro, macroSendoDefinida);
-                 } else if (tabelaDeMacros.containsKey(opcode)) { // ser for uma chamada de macro
+                if (tabelaDeMacros.containsKey(opcode)) { // ser for uma chamada de macro
                     Macro macroSendoExpandida = tabelaDeMacros.get(opcode);
                     String[] tokensChamada = linha.trim().split("\\s+");
-                    String[] argumentosDaMacroSendoExpandida = Arrays.copyOfRange(tokensChamada, 1, Math.max(2, tokensChamada.length));
+                    String[] argumentosDaMacroSendoExpandida = Arrays.copyOfRange(tokensChamada, 1,
+                            Math.max(2, tokensChamada.length));
 
-                    expandirMacro(macroSendoExpandida, argumentosDaMacroSendoExpandida, outStream, númeroDeExpansões++);
-                 } else { // se não é definição nem chamada de macro
+                    expandirMacro(macroSendoExpandida, argumentosDaMacroSendoExpandida, outStream,
+                            númeroDeExpansões++);
+                } else { // se não é definição nem chamada de macro
                     outStream.append(linha.trim() + "\n");
-                 }
+                }
             }
+
         }
         System.out.println(tabelaDeMacros);
     }
@@ -80,7 +97,7 @@ public class ProcessadorDeMacros {
             String linha = macroScanner.nextLine();
             Scanner lineScanner = new Scanner(linha);
 
-            while(lineScanner.hasNext()) {
+            while (lineScanner.hasNext()) {
                 String token = lineScanner.next();
                 if (token.charAt(0) == '#') {
                     int índiceArgumento = Integer.parseInt(token.substring(1));
@@ -113,10 +130,13 @@ public class ProcessadorDeMacros {
 
         }
         scanner.close();
+        if (novaLinha.trim().equals("MACRO")) {
+            return "*";
+        }
         return novaLinha.trim();
     }
 
-    public static void main(String[] args) throws FileNotFoundException  {
+    public static void main(String[] args) throws FileNotFoundException {
         ProcessadorDeMacros.executar(new File("input.txt"), new File("MASMAPRG.ASM"));
     }
 }
