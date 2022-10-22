c29eb4cf87d46c052b1cab8b21186476727f367c
João Rezende <joaozinhobent@gmail.com>
2022-10-22 12:46:10 -0300

    ProcMacros: refatoração geral

--- ProcessadorDeMacros.java
+++ ProcessadorDeMacros.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -38,16 +37,16 @@ public class ProcessadorDeMacros {
         PrintStream outStream = new PrintStream(moduloSaida);
 
         int nivelDeDefinição = 0;
+        boolean modoDeExpansão = false;
+        String[] argumentosDaMacroSendoExpandida = null;
+        // Scanner da macro que está sendo expandida, se estamos no modo de expansão.
+        Scanner macroScanner = null;
         HashMap<String, Macro> tabelaDeMacros = new HashMap<>();
         Macro macroSendoDefinida = null;
         int númeroDeExpansões = 0;
         Stack<Parâmetro> pilhaDeParâmetros = new Stack<>();
-        while (fileScanner.hasNext()) {
-            String linha = fileScanner.nextLine();
-            if (linha.trim().equals("*")) {
-                continue;
-            }
-
+        String linha = fileScanner.nextLine();
+        while (!linha.equals("")) {
             Scanner lineScanner = new Scanner(linha);
 
             String opcode = lineScanner.next();
@@ -57,91 +56,139 @@ public class ProcessadorDeMacros {
                 opcode = lineScanner.next();
             }
 
-            if (opcode.equals("MACRO")) {
-                nivelDeDefinição += 1;
-                String protótipo = fileScanner.nextLine();
-                String[] tokensPrototipo = protótipo.trim().split("\\s+");
+            while (true) {
+                if (opcode.equals("MACRO")) {
+                    nivelDeDefinição += 1;
+                    String protótipo = fileScanner.nextLine();
+                    String[] tokensPrototipo = protótipo.trim().split("\\s+");
 
-                String nomeDaMacro = tokensPrototipo[0];
-                String[] parâmetros = Arrays.copyOfRange(tokensPrototipo, 1, Math.max(2, tokensPrototipo.length));
-                for (int i = 0; i < parâmetros.length; i++) {
-                    pilhaDeParâmetros.add(new Parâmetro(parâmetros[i], nivelDeDefinição, i));
-                }
-
-                if (nivelDeDefinição == 1) {
-                    macroSendoDefinida = new Macro(parâmetros);
-                    tabelaDeMacros.put(nomeDaMacro, macroSendoDefinida);
-                } else {
-                    macroSendoDefinida.corpo += "MACRO\n";
-                    macroSendoDefinida.corpo += protótipo + "\n";
-                }
-            }
-
-            if (nivelDeDefinição > 0) {
-                if (opcode.equals("MEND")) {
-                    while (pilhaDeParâmetros.size() > 0 && pilhaDeParâmetros.peek().nível == nivelDeDefinição) {
-                        pilhaDeParâmetros.pop();
+                    String nomeDaMacro = tokensPrototipo[0];
+                    String[] parâmetros = Arrays.copyOfRange(tokensPrototipo, 1, Math.max(2, tokensPrototipo.length));
+                    for (int i = 0; i < parâmetros.length; i++) {
+                        pilhaDeParâmetros.add(new Parâmetro(parâmetros[i], nivelDeDefinição, i));
                     }
-                    nivelDeDefinição -= 1;
-                } else if (nivelDeDefinição > 1) {
-                    macroSendoDefinida.corpo += substituiReferenciasAParâmetros(linha, macroSendoDefinida.parâmetros, pilhaDeParâmetros)
-                            + "\n";
+
+                    if (nivelDeDefinição == 1) {
+                        macroSendoDefinida = new Macro(parâmetros);
+                        tabelaDeMacros.put(nomeDaMacro, macroSendoDefinida);
+                    } else {
+                        macroSendoDefinida.corpo += "MACRO\n";
+                        macroSendoDefinida.corpo += protótipo + "\n";
+                    }
+                    break;
                 }
 
-                if (opcode.equals("MEND") && nivelDeDefinição > 0) {
-                    macroSendoDefinida.corpo += "MEND\n";
-                }
-
-            } else { // se não estamos em modo de definição
-                if (tabelaDeMacros.containsKey(opcode)) { // ser for uma chamada de macro
+                // ser for uma chamada de macro
+                if (tabelaDeMacros.containsKey(opcode)) {
+                    modoDeExpansão = true;
+                    númeroDeExpansões += 1;
                     Macro macroSendoExpandida = tabelaDeMacros.get(opcode);
+                    macroScanner = new Scanner(macroSendoExpandida.corpo);
                     String[] tokensChamada = linha.trim().split("\\s+");
-                    String[] argumentosDaMacroSendoExpandida = Arrays.copyOfRange(tokensChamada, 1,
+                    argumentosDaMacroSendoExpandida = Arrays.copyOfRange(tokensChamada, 1,
                             Math.max(2, tokensChamada.length));
+                }
 
-                    expandirMacro(macroSendoExpandida, argumentosDaMacroSendoExpandida, outStream,
-                            númeroDeExpansões++);
-                } else { // se não é definição nem chamada de macro
+                if (opcode.equals("MEND")) {
+                    argumentosDaMacroSendoExpandida = null;
+                    if (nivelDeDefinição == 0) {
+                        modoDeExpansão = false;
+                    } else if (!modoDeExpansão) {
+                        // desempilhar parâmetros do nível de definição do qual estamos saindo
+                        while (pilhaDeParâmetros.size() > 0 && pilhaDeParâmetros.peek().nível == nivelDeDefinição) {
+                            pilhaDeParâmetros.pop();
+                        }
+                        nivelDeDefinição -= 1;
+                        macroSendoDefinida.corpo += "MEND\n";
+                    }
+                    break;
+                }
+
+                if (modoDeExpansão) {
+                    linha = substituirReferênciasAArgumentos(linha, argumentosDaMacroSendoExpandida);
+                    linha = cocatenarContadorARótulos(linha, númeroDeExpansões);
+                }
+                if (!modoDeExpansão && nivelDeDefinição > 0) {
+                    linha = substituiReferenciasAParâmetros(linha,
+                            macroSendoDefinida.parâmetros,
+                            pilhaDeParâmetros);
+                }
+                if (nivelDeDefinição == 0) {
                     outStream.append(linha.trim() + "\n");
+                } else {
+                    macroSendoDefinida.corpo += linha.trim() + "\n";
+                }
+                break;
+            }
+
+            lineScanner.close();
+
+            // ler próxima linha
+            if (modoDeExpansão) {
+                if (macroScanner.hasNext()) {
+                    linha = macroScanner.nextLine();
+                } else {
+                    modoDeExpansão = false;
                 }
             }
-            lineScanner.close();
+            if (!modoDeExpansão) {
+                linha = fileScanner.nextLine();
+                while (linha.trim().equals("*")) {
+                    linha = fileScanner.nextLine();
+                }
+
+            }
         }
+
         System.out.println(tabelaDeMacros);
         fileScanner.close();
+        outStream.close();
+
     }
 
-    static void expandirMacro(Macro macro, String[] argumentos, PrintStream outStream, int contadorDeExpansões) {
-        Scanner macroScanner = new Scanner(macro.corpo);
-        while (macroScanner.hasNext()) {
-            String linha = macroScanner.nextLine();
-            Scanner lineScanner = new Scanner(linha);
-            while (lineScanner.hasNext()) {
-                String token = lineScanner.next();
-                if (token.charAt(0) == '#') {
-                    Scanner tokenScanner = new Scanner(token.substring(2, token.length() - 1));
-                    tokenScanner.useDelimiter(",");
-                    int nívelDeDefinição = tokenScanner.nextInt();
-                    int índiceArgumento = tokenScanner.nextInt();
-                    tokenScanner.close();
-                    outStream.append(argumentos[índiceArgumento - 1] + (lineScanner.hasNext() ? " " : ""));
+    static String substituirReferênciasAArgumentos(String linha, String[] argumentos) {
+        String linhaNova = "";
+        Scanner lineScanner = new Scanner(linha);
+        while (lineScanner.hasNext()) {
+            String token = lineScanner.next();
+            if (token.charAt(0) == '#') {
+                Scanner tokenScanner = new Scanner(token.substring(2, token.length() - 1));
+                tokenScanner.useDelimiter(",");
+                int nívelDeDefinição = tokenScanner.nextInt();
+                int índiceArgumento = tokenScanner.nextInt();
+                tokenScanner.close();
+
+                if (nívelDeDefinição == 1) {
+                    linhaNova += argumentos[índiceArgumento - 1] + (lineScanner.hasNext() ? " " : "");
                 } else {
-                    token = token.replace(".SER", Integer.valueOf(contadorDeExpansões).toString());
-                    outStream.append(token + (lineScanner.hasNext() ? " " : ""));
+                    linhaNova += "#(" + (nívelDeDefinição - 1) + "," + índiceArgumento + ")"
+                            + (lineScanner.hasNext() ? " " : "");
                 }
             }
-            outStream.append("\n");
-            lineScanner.close();
         }
-        macroScanner.close();
+        lineScanner.close();
+        return linhaNova;
     }
 
-    static String substituiReferenciasAParâmetros(String linha, String[] parâmetros, Stack<Parâmetro> pilhaDeParâmetros) {
+    static String cocatenarContadorARótulos(String linha, int contadorDeExpansões) {
+        String linhaNova = "";
+        Scanner lineScanner = new Scanner(linha);
+        while (lineScanner.hasNext()) {
+            String token = lineScanner.next();
+            token = token.replace(".SER", Integer.valueOf(contadorDeExpansões).toString());
+            linhaNova += token + (lineScanner.hasNext() ? " " : "");
+        }
+        lineScanner.close();
+        return linhaNova;
+    }
+
+    static String substituiReferenciasAParâmetros(String linha, String[] parâmetros,
+            Stack<Parâmetro> pilhaDeParâmetros) {
         String novaLinha = "";
         Scanner scanner = new Scanner(linha);
         while (scanner.hasNext()) {
             String token = scanner.next();
-            
+
             Parâmetro parâmetro = null;
             for (int i = pilhaDeParâmetros.size() - 1; i >= 0; i--) {
                 if (pilhaDeParâmetros.get(i).nome.equals(token)) {
